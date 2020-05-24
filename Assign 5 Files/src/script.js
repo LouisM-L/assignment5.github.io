05d4fa24dc1a6b8f8e030c43aff25d0064283b2d
Louis Messier-Lavallée <LouisM-L@users.noreply.github.com>
2020-05-23 21:42:12 -0400

    added new meal function

--- Assign 5 Files/src/script.js
+++ Assign 5 Files/src/script.js
@@ -1,13 +1,24 @@
+var loggedin = false
 function login()
 {
    if(document.getElementById("log in").innerHTML === "LOG IN")
    {
        document.getElementById("log in").innerHTML = "LOG OUT";
        console.log("logged in");
+       loggedin = true;
+       
    }
    else
    {
        document.getElementById("log in").innerHTML = "LOG IN";
        console.log("logged out");
    }
+}
+
+function NewMeal()
+{
+    if(loggedin === true)
+    {
+        
+    }
 }
